ec1e0d66efe68e7324cc86cfcac711a85ae43aee
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-09-20 20:11:47 +0000

    update linters config files

--- main.js
+++ main.js
@@ -1,69 +1,52 @@
 const displaySection = document.querySelector('.book_list');
-const authorText = document.getElementById('author');
-const titleText = document.getElementById('title');
-const addBtn = document.getElementById('add_btn');
+const addBtn = document.querySelector('#add_btn');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
 
 let availableBooks = [];
-
-const addBook = (element) => {
-  element.preventDefault();
-  const added_Book = {
-    title: title.value,
-    author: author.value,
-  };
-
-  availableBooks.push(added_Book);
-  clear();
-  saveToLocalStorage(availableBooks);
-  displayItem();
-
-};
 
 const clear = () => {
   title.value = '';
   author.value = '';
 };
 
-/* add button */
-addBtn.addEventListener('click', addBook);
-document.addEventListener('DOMContentLoaded', () => {
-  displayItem();
-});
-
 /* save to localStorage */
 
-const saveToLocalStorage = (availableBooks) => 
-localStorage.setItem('availableBooks', JSON.stringify(availableBooks));
+const saveToLocalStorage = (availableBooks) => localStorage.setItem('availableBooks', JSON.stringify(availableBooks));
 
 /* get from localStorage */
 
 const getFromLocalStorage = () => {
-  if (JSON.parse(localStorage.getItem('availableBooks'))) 
-  availableBooks = JSON.parse(localStorage.getItem('availableBooks'));
+  if (JSON.parse(localStorage.getItem('availableBooks'))) { availableBooks = JSON.parse(localStorage.getItem('availableBooks')); }
 };
 
-/* display items*/
+const deleteBook = (index) => {
+  const BookList = availableBooks.filter((book) => book !== availableBooks[index]);
+  saveToLocalStorage(BookList);
+  displayItem();
+};
+
+/* display items */
 const displayItem = () => {
   getFromLocalStorage();
   displaySection.innerHTML = '';
 
   availableBooks.forEach((availableBook, index) => {
-
     displaySection.innerHTML += `
     <div class="availableBook">
       <p>${availableBook.title}</p>
       <p>${availableBook.author}</p>
     </div> `;
 
-    /*  Delete btn created*/
+    /*  Delete btn created */
     const removeBtn = document.createElement('button');
     removeBtn.textContent = 'Remove';
     removeBtn.classList.add('remove');
     displaySection.appendChild(removeBtn);
     displaySection.innerHTML += `
     <hr>
-    `
-    /*  Delete btn remove function*/
+    `;
+    /*  Delete btn remove function */
     const deleteBtn = document.querySelector('.remove');
     deleteBtn.addEventListener('click', () => {
       deleteBook(index);
@@ -71,9 +54,21 @@ const displayItem = () => {
   });
 };
 
-const deleteBook = (index) => {
-  const BookList = availableBooks.filter((book) => 
-  book !== availableBooks[index]);
-  saveToLocalStorage(BookList);
+const addBook = (element) => {
+  element.preventDefault();
+  const addedBook = {
+    title: title.value,
+    author: author.value,
+  };
+
+  availableBooks.push(addedBook);
+  clear();
+  saveToLocalStorage(availableBooks);
   displayItem();
 };
+
+/* add button */
+addBtn.addEventListener('click', addBook);
+document.addEventListener('DOMContentLoaded', () => {
+  displayItem();
+});
